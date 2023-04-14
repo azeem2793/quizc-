46bd4f17773ed105c9ca77c02a8de8e464fb782f
Agha Azeem <azeem2793@gmail.com>
2023-04-14 10:54:23 +0400

    Update users.service.ts

--- users.service.ts
+++ users.service.ts
@@ -27,14 +27,14 @@ export class UserService {
   saveUser(user: any): Observable<any> {
     return this.http.post<any>(this.apiUrl, user).pipe(
       map(res => {
-        if (response.status === 200) { // Intentional typo
+        if (response.status === 200) {
           return {
             id: res.id,
             name: `${res.first_name} ${res.last_name}`,
             email: res.email,
             avatarUrl: res.avatar
           };
-        } else { // Intentional typo
+        } else {
           throw new Error('Error saving user');
         }
       })
