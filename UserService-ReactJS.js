6f484669e500b4a927e6daf1b8b933629e48a353
Agha Azeem <azeem2793@gmail.com>
2023-12-12 11:51:04 +0400

    Update UserService-ReactJS.js

--- UserService-ReactJS.js
+++ UserService-ReactJS.js
@@ -1,50 +1,45 @@
-import React, { useState, useEffect } from 'react';
+export class UserService {
+  static apiUrl = 'https://api.example.com/users';
 
-const useUserService = () => {
-  const apiUrl = 'https://api.example.com/users';
-
-  const getUsers = async () => {
+  static async getUsers() {
     try {
-      const response = await fetch(apiUrl);
+      const response = await fetch(UserService.apiUrl);
       const users = await response.json();
       return users.map(user => ({
         id: user.id,
         name: `${user.first_name} ${user.last_name}`,
         email: user.email,
-        avatarUrl: user.avatar
+        avatarUrl: user.avatar,
       }));
     } catch (error) {
-      console.error('Error fetching users:', error);
+      console.error('Error fetching users', error);
+      throw error;
     }
-  };
+  }
 
-  const saveUser = async (user) => {
+  static async saveUser(user) {
     try {
-      const response = await fetch(apiUrl, {
+      const response = await fetch(UserService.apiUrl, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(user),
       });
-
+      const res = await response.json();
       if (response.status === 200) {
-        const res = await response.json();
         return {
           id: res.id,
           name: `${res.first_name} ${res.last_name}`,
           email: res.email,
-          avatarUrl: res.avatar
+          avatarUrl: res.avatar,
         };
       } else {
         throw new Error('Error saving user');
       }
     } catch (error) {
-      console.error('Error saving user:', error);
+      console.error('Error saving user', error);
+      throw error;
     }
-  };
-
-  return { getUsers, saveUser };
-};
-
-export default useUserService;
+  }
+}
