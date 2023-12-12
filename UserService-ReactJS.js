2d03fe645dc2ad58aaab7e4cf80c27a814fa02a5
Agha Azeem <azeem2793@gmail.com>
2023-12-12 12:02:11 +0400

    Update UserService-ReactJS.js

--- UserService-ReactJS.js
+++ UserService-ReactJS.js
@@ -1,45 +1,37 @@
+// userService.js
+
 export class UserService {
   static apiUrl = 'https://api.example.com/users';
 
   static async getUsers() {
-    try {
-      const response = await fetch(UserService.apiUrl);
-      const users = await response.json();
-      return users.map(user => ({
-        id: user.id,
-        name: `${user.first_name} ${user.last_name}`,
-        email: user.email,
-        avatarUrl: user.avatar,
-      }));
-    } catch (error) {
-      console.error('Error fetching users', error);
-      throw error;
-    }
+    const response = await fetch(UserService.apiUrl);
+    const users = await response.json();
+    return users.map(user => ({
+      id: user.id,
+      name: `${user.first_name} ${user.last_name}`,
+      email: user.email,
+      avatarUrl: user.avatar,
+    }));
   }
 
   static async saveUser(user) {
-    try {
-      const response = await fetch(UserService.apiUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      });
-      const res = await response.json();
-      if (response.status === 200) {
-        return {
-          id: res.id,
-          name: `${res.first_name} ${res.last_name}`,
-          email: res.email,
-          avatarUrl: res.avatar,
-        };
-      } else {
-        throw new Error('Error saving user');
-      }
-    } catch (error) {
-      console.error('Error saving user', error);
-      throw error;
+    const response = await fetch(UserService.apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+    const res = await response.json();
+    if (response.status === 200) {
+      return {
+        id: res.id,
+        name: `${res.first_name} ${res.last_name}`,
+        email: res.email,
+        avatarUrl: res.avatar,
+      };
+    } else {
+      throw new Error('Error saving user');
     }
   }
 }
